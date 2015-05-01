3d06d8461880c7f50218b20ded9c1fd7f4a26961
vlasov-id-131216 <vlasovi.ilya@gmail.com>
2015-05-01 19:55:01 +0300

    че то поделал 2

--- src/main/main.py
+++ src/main/main.py
@@ -6,18 +6,21 @@
 """
 
 # TODO: Подгружать пути из конфигов
-# TODO: Сделать выбор способа анализа шва (перпендикуляр, окружность, оба)
+# TODO: Сделать выбор способа анализа шва (оба)
 
 
 import sys
 import argparse
 import src.utils.scope.scope as s
-import src.preparation as preparation
 import src.edge_detection.canny as canny
-import src.defect_detection.check_position_by_perpendicular as check_position
-import src.defect_detection.check_width_by_perpendicular as check_width
+import src.defect_detection.check_position_by_perpendicular as check_position_by_perpendicular
+import src.defect_detection.check_width_by_perpendicular as check_width_by_perpendicular
+import src.defect_detection.check_position_by_perpendicular as check_position_by_area
+import src.defect_detection.check_width_by_perpendicular as check_width_by_area
 import src.defect_detection.analyze_results as analyze_results
 from src.utils.validators.args_validator import args_validator
+from src.preparation import interpolate
+from src.preparation import area
 
 # Добавление модулей приложения в sys.path
 sys.path.append(u'/home/vlasov-id-131216/Dropbox/Универ/Диплом/project/DefectDetection/')
@@ -30,13 +33,14 @@ def main_console(args):
     :param args: Параметры для работы приложения (фотография детали, область для сканирования и т.д.
     :return: Инструкция к дальнейшим действиям
     """
+
     # Подготовка исходных данных
-    area = s.Rectangle(s.Point(args.area[0], args.area[1]),
-                       s.Point(args.area[2], args.area[3]))
+    rectangle = s.Rectangle(s.Point(args.area[0], args.area[1]),
+                            s.Point(args.area[2], args.area[3]))
     point = s.Point(args.point[0], args.point[1])
     scope = s.Scope()
     scope.load_image_by_path(args.image)
-    scope.select_area(area)
+    scope.select_area(rectangle)
     scope.select_point(point)
     scope.set_area_color(255, 0, 0)
     scope.set_point_color(0, 0, 255)
@@ -46,12 +50,15 @@ def main_console(args):
     canny.start(scope)
 
     # Определение области для сканирования (линия или круг)
-    if True:
-        perpendicular = preparation.interpolate.get_perpendicular(scope)
+    if args.type == 1:
+        perpendicular = interpolate.get_perpendicular(scope)
 
-        check_position_result = check_position.check(scope, perpendicular)
-        check_width_result = check_width.check(scope, perpendicular)
-    else:
+        check_position_result = check_position_by_perpendicular.check(scope, perpendicular)
+        check_width_result = check_width_by_perpendicular.check(scope, perpendicular)
+    elif args.type == 2:
+        a = area.get_area(scope)
+        check_position_result = check_position_by_area.check(scope, a)
+        check_width_result = check_width_by_area.check(scope, a)
         pass
 
     # Анализ полученных результатов
@@ -65,6 +72,7 @@ if __name__ == '__main__':
     parser.add_argument('-a', '--area',  help='work area (-a x1 y1 x2 y2)',  nargs='+', type=int)
     parser.add_argument('-p', '--point', help='point (-p x y)',              nargs='+', type=int)
     parser.add_argument('-s', '--size',  help='image size in mm (-i h w)',   nargs='+', type=float)
+    parser.add_argument('-t', '--type',  help='1 - perpendicular; 2 - area; 3 - both',  type=int)
     _args = parser.parse_args()
     if _args.mode == 'console':
         main_console(_args)
